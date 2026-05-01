c1c2eda6f179722e9bd16de2b8a5599f988ce213
kalindafab <kfablionel@gmail.com>
2026-05-01 11:32:27 +0200

    adding hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-web3-v4"; 
+import "@nomicfoundation/hardhat-web3-v4";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
   networks: {
-    hardhat: {}, 
+    hardhat: {},
     amoy: {
       url: process.env.POLYGON_AMOY_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
-     sepolia: {
+    sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
